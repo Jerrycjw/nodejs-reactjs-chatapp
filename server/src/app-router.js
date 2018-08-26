337791aed9952f81810f8e4fe243d7fbccddcf10
jerrycjw <YOUR EMAIL ADDRESS>
2018-08-26 11:49:48 -0700

    move to public

--- server/src/app-router.js
+++ server/src/app-router.js
@@ -26,7 +26,7 @@ export default class AppRouter {
         console.log("APp ROuter works!");
 
         app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname+'/app/build/index.html'));
+            res.sendFile(path.join(__dirname+'/public/index.html'));
         });
 
         app.get('/api/', (req, res, next) => {
