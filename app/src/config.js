6ce44517bca782e460675965b251909ca3113679
jerrycjw <YOUR EMAIL ADDRESS>
2018-08-25 23:28:28 -0700

    update domain

--- app/src/config.js
+++ app/src/config.js
@@ -1,6 +1,6 @@
 export const production = false; // set it to true when deploy to the server
 
 // const domain = production ? '139.59.227.127' : '127.0.0.1:3001'; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
-export const domain = "serene-mesa-51474.herokuapp.com/"
+export const domain = "serene-mesa-51474.herokuapp.com"
 export const websocketUrl = `wss://${domain}`
 export const apiUrl = `https://${domain}`
