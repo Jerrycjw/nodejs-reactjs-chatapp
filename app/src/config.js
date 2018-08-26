8702096a740b57e5488eb6ee0c7602cb6d945f24
jerrycjw <YOUR EMAIL ADDRESS>
2018-08-25 21:28:33 -0700

    update ws

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,5 @@
 export const production = false; // set it to true when deploy to the server
 
 const domain = production ? '139.59.227.127' : '127.0.0.1:3001'; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
-export const websocketUrl = `ws://${domain}`
-export const apiUrl = `http://${domain}`
+export const websocketUrl = location.origin.replace(/^http/, 'ws');
+export const apiUrl = location.origin;
