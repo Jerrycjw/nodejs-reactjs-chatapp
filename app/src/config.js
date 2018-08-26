337a2e998c6574217682a727330f3ec39ebf4ded
jerrycjw <YOUR EMAIL ADDRESS>
2018-08-25 21:34:03 -0700

    update domain

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,6 @@
 export const production = false; // set it to true when deploy to the server
 
-const domain = production ? '139.59.227.127' : '127.0.0.1:3001'; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
-export const websocketUrl = location.origin.replace(/^http/, 'ws');
-export const apiUrl = location.origin;
+// const domain = production ? '139.59.227.127' : '127.0.0.1:3001'; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
+export const domain = "sleepy-eyrie-64329.herokuapp.com/"
+export const websocketUrl = `ws://${domain}`
+export const apiUrl = `http://${domain}`
